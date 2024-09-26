889e846f53f9430a958aa8f79af242ae08f9979f
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-09-26 16:54:57 +0500

    Update page.js - DashBoard
    Make the page more dynamic, but giving dynamic active tabs style, navbar hidden, profile icon dropdown.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import classes from "./page.module.css";
 import logoIcon from "@/public/assets/logo.svg";
@@ -8,45 +10,102 @@ import hamburgerIcon from "@/public/assets/icons/hamburger.svg";
 import profileIcon from "@/public/assets/icons/profile.svg";
 import bellIcon from "@/public/assets/icons/bell.svg";
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 export default function DashBoardPage() {
+  const [activeItem, setActiveItem] = useState("home");
+  const [showNav, setShowNav] = useState(true);
+  const [showProfileDropdown, setShowProfileDropdown] = useState(false);
+  const navRef = useRef(null);
+  const mainRef = useRef(null);
+
+  function hideNavHandler() {
+    if (!showNav) {
+      navRef.current.style.display = "none";
+      mainRef.current.style.width = "100%";
+    } else {
+      navRef.current.style.display = "flex";
+      mainRef.current.style.width = "80%";
+    }
+    setShowNav(!showNav);
+  }
+
   return (
     <div className={classes.container}>
-      <div className={classes.nav}>
+      <div className={classes.nav} ref={navRef}>
         <div className={classes.navBody}>
           <div className={classes.logo}>
             <Image src={logoIcon} />
             <h1>ExamBoard</h1>
           </div>
           <ul className={classes.ul}>
-            <li className={`${classes.li} ${classes.active}`}>
-              <Image src={homeIcon} />
+            <li
+              className={`${classes.li} ${
+                activeItem === "home" ? classes.active : ""
+              }`}
+              onClick={() => setActiveItem("home")}
+            >
+              <Image src={homeIcon} alt="Home" />
               <Link href="/">Home</Link>
             </li>
-            <li className={classes.li}>
-              <Image src={usersIcon} />
+            <li
+              className={`${classes.li} ${
+                activeItem === "users" ? classes.active : ""
+              }`}
+              onClick={() => setActiveItem("users")}
+            >
+              <Image src={usersIcon} alt="Users" />
               <Link href="/">Users</Link>
             </li>
           </ul>
         </div>
 
-        <div className={classes.settings}>
-          <Image src={settingsIcon} />
-          <Link href="/">Settings</Link>
+        <div className={classes.settingsContanier}>
+          <div
+            className={`${classes.settings} ${
+              activeItem === "settings" ? classes.active : ""
+            }`}
+          >
+            <Image src={settingsIcon} />
+            <Link href="/" onClick={() => setActiveItem("settings")}>
+              Settings
+            </Link>
+          </div>
         </div>
       </div>
-      <div className={classes.main}>
+      <div className={classes.main} ref={mainRef}>
         <header className={classes.header}>
-          <Image src={hamburgerIcon} />
+          <Image src={hamburgerIcon} onClick={hideNavHandler} />
           <div className={classes.profilers}>
-            <Image src={bellIcon} />
+            <Image src={bellIcon} className={classes.rightPadding} />
+            {showProfileDropdown && (
+              <p className={classes.username}>User's Name</p>
+            )}
             <div className={classes.userIcon}>
-              <Link href='/auth/login'>
-                <Image src={profileIcon} />
-              </Link>
+              {/* <Link href="/auth/login"> */}
+              <Image
+                src={profileIcon}
+                onClick={() => setShowProfileDropdown(!showProfileDropdown)}
+              />
+              {/* </Link> */}
             </div>
           </div>
         </header>
+        <main>
+          {showProfileDropdown && (
+            <div className={classes.profileDropdown}>
+              <Link className={classes.profileList} href="/auth/forgotpassword">
+                Change Password
+              </Link>
+              <Link
+                className={`${classes.profileList} ${classes.topBorder}`}
+                href="/auth/login"
+              >
+                Log out
+              </Link>
+            </div>
+          )}
+        </main>
       </div>
     </div>
   );
